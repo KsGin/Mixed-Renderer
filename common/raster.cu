7086b7977484b90e68ed8808494e38e93e7e35f7
KsGin <imqqyangfan@gmail.com>
2018-11-28 09:40:21 +0800

    update

---
+++ common/raster.cu
@@ -0,0 +1,17 @@
+/**
+ * File Name : raster.cu
+ * Author : Yang Fan
+ * Date : 2018/11/28
+ * declare raster
+ */
+
+#include "device.cu"
+
+class Raster
+{
+private:
+	Device device;
+
+public:
+	Raster() {}
+};
