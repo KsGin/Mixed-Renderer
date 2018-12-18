f773fcd44246ff5bc1a74058db78dd6ec52183c3
KsGin <imqqyangfan@gmail.com>
2018-12-18 22:28:26 +0800

    Update transform

--- main.cpp
+++ main.cpp
@@ -8,8 +8,10 @@
 #include "cuda/define.cu"
 #include "common/device.h"
 #include "common/raster.h"
+#include "includes/math/matrix.hpp"
 
 using namespace std;
+using namespace Math;
 
 /**
  * sdl defined main to SDL_main , so we undef it on here
@@ -31,7 +33,19 @@ int main()
 
 	while (!d->windowShouldClose()) {
 
-		r->draw(Math::Vector2(-0.2, 0.4), Math::Vector2(-0.4, 0), Math::Vector2(0, 0), Color::green() , Raster::WIREFRAME);
+		Matrix model = Matrix::identity();
+		Matrix view = Matrix::lookAtLH(Vector3(0, 0, -2), Vector3(0, 0, 0), Vector3(0, 1, 0));
+		Matrix perspective = Matrix::perspectiveFovLH(60, SCREEN_WIDTH / SCREEN_HEIGHT, 0.01, 1000);
+
+		Matrix mvp = Matrix::transpose(model * view * perspective);
+
+		Vector3 p1 = Matrix::transformCoordinates(Vector3(-1, 0, 0), mvp);
+		Vector3 p2 = Matrix::transformCoordinates(Vector3(0, 2, 0), mvp);
+		Vector3 p3 = Matrix::transformCoordinates(Vector3(1, 0, 0), mvp);
+
+		// printf("%f %f %f\n", p2._x, p1._y, p1._z);
+
+		r->draw(Vector2(p1._x, p1._y), Vector2(p2._x, p2._y), Vector2(p3._x, p3._y), Color::red() , Raster::SOLID);
 
 		d->handleEvent();
 		d->updateRender();
