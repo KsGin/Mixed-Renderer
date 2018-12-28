5b7a7f85544211097b56f88578b6f71bdd78920a
KsGin <imqqyangfan@gmail.com>
2018-12-28 18:56:59 +0800

    Fixed bug.1

--- main.cpp
+++ main.cpp
@@ -33,7 +33,7 @@ int main()
 	Matrix view = Matrix::lookAtLH(Vector3(0, 0, 10), Vector3(0, 0, 0), Vector3(0, 1, 0));
 	Matrix perspective = Matrix::perspectiveFovLH( 1 , SCREEN_WIDTH / SCREEN_HEIGHT, 0.1, 1000);
 
-	Model m = Model::triangle();
+	Model m = Model::cube();
 
 	Shader shader;
 	shader.setMat(view, Shader::MatType::VIEW);
@@ -44,7 +44,7 @@ int main()
 	while (!d.windowShouldClose()) {
 		d.clear();
 
-		//model = model * Math::Matrix::rotationY(-0.02f) * Math::Matrix::rotationZ(-0.02f) * Math::Matrix::rotationX(-0.02f);
+		model = model * Math::Matrix::rotationY(-0.02f) * Math::Matrix::rotationZ(-0.02f) * Math::Matrix::rotationX(-0.02f);
 		shader.setMat(model, Shader::MatType::MODEL);
 
 		Render::render(m, shader, Raster::SOLID);
