2d7bb8135e6b6310ef0859169dd2257ebc28457e
KsGin <imqqyangfan@gmail.com>
2019-05-14 11:16:35 +0800

    调整摄像机视角

--- main.cpp
+++ main.cpp
@@ -31,18 +31,19 @@ int main()
 	Renderer::initialize(NUM_PIXELS , NUM_COLORS , NUM_TRIANGLES);
 	auto r = Renderer::getInstance();
 
-	auto cubeModel = Matrix::identity() * Matrix::scale(0.1 , 0.1 , 0.1);
-	auto waterModel = Matrix::identity() * Matrix::scale(1.5 , 1 , 1.0);
+	auto cubeModel1 = Matrix::identity() * Matrix::scale(0.1 , 0.1 , 0.1);
+	auto cubeModel2 = Matrix::identity() * Matrix::scale(0.1 , 0.1 , 0.1);
+	auto waterModel = Matrix::identity() * Matrix::scale(1.5 , 1 , 1.5);
 	auto lightModel =  Matrix::identity() * Matrix::scale( 0.02 , 0.02 , 0.02);
 
-	auto camera = PerspectiveCamera(90 , Vector3(0, 3, 5) , Vector3(0, 0, 0) , Vector3(0, 1, 0) , 0.1 , 1000);
+	auto camera = PerspectiveCamera(60 , Vector3(0, 5, 5) , Vector3(0, 0, 0) , Vector3(0, 1, 0) , 0.1 , 100);
 	r.setPerspectiveCamera(camera);
 
 	auto rotation1 = Matrix::identity();
 	auto rotation2 = Matrix::identity();
 
 	auto view = Matrix::lookAtLH(camera.eye, camera.target, Vector3(0, 1, 0));
-	auto perspective = Matrix::perspectiveFovLH( 1.5 , SCREEN_WIDTH / SCREEN_HEIGHT, camera.near, camera.far);
+	auto perspective = Matrix::perspectiveFovLH( 1 , SCREEN_WIDTH / SCREEN_HEIGHT, camera.near, camera.far);
 
 	auto cube1 = Model::cube();
 	auto cube2 = Model::cube();
@@ -53,10 +54,10 @@ int main()
 	cubeShader.setMat(view, VIEW);
 	cubeShader.setMat(perspective, PERSPECTIVE);
 
-	auto waterShader = Shader(WATER);
-	waterShader.setMat(view , VIEW);
-	waterShader.setMat(perspective , PERSPECTIVE);
-
+	auto floorShader = Shader(WATER);
+	floorShader.setMat(view , VIEW);
+	floorShader.setMat(perspective , PERSPECTIVE);
+	
 	auto lightShader = Shader(LIGHT);
 	lightShader.setMat(view ,VIEW);
 	lightShader.setMat(perspective , PERSPECTIVE);
@@ -76,18 +77,18 @@ int main()
 		rotation2 = rotation2 * Matrix::rotationY( 0.02f) * Matrix::rotationZ( 0.02f) * Matrix::rotationX( 0.02f);
 
 
-		cubeShader.setMat(cubeModel * rotation1 * Matrix::translate(0.5 , 1 , 0), MODEL);
+		cubeShader.setMat(cubeModel1 * rotation1 * Matrix::translate(0.6 , 1 , 0), MODEL);
 		r.add(cube1 , cubeShader , 0 , SOLID);
 
-		cubeShader.setMat(cubeModel * rotation2 * Matrix::translate(-0.5 , 1 , 0), MODEL);
+		cubeShader.setMat(cubeModel2 * rotation2 * Matrix::translate(-0.6 , 1 , 0), MODEL);
 		r.add(cube2 , cubeShader , 0 , SOLID);
 
-		args.bis += bis;
-		if (args.bis >= 0.1f || args.bis <= -0.1f) {bis = -bis;}
-		waterShader.setArgs(args);
+		// args.bis += bis;
+		// if (args.bis >= 0.1f || args.bis <= -0.1f) {bis = -bis;}
+		// waterShader.setArgs(args);
 
-		waterShader.setMat(waterModel * Matrix::translate(0 , 0 , 0.6), MODEL);
-		r.add(floor , waterShader ,0.5 , SOLID);
+		floorShader.setMat(waterModel * Matrix::translate(0 , 0 , 0.5), MODEL);
+		r.add(floor , floorShader ,0.5 , SOLID);
 
 		//
 		lightShader.setMat(lightModel * rotation1 * Matrix::translate(0 , 2 , 0) , MODEL);
